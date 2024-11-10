0cbfb5a8a529c65d0d360130c4e4a00f964f6ee6
Максим Брейлян <maxim.breilyan@yandex.ru>
2024-11-10 16:12:54 +0300

    + первый коммит дипломной работы

--- staticstic-service/statistic-dto/src/main/java/dto/RequestDto.java
+++ staticstic-service/statistic-dto/src/main/java/dto/RequestDto.java
@@ -16,9 +16,13 @@ import java.time.LocalDateTime;
 public class RequestDto {
 
     private Long id;
+    @NotNull
     private String app;
+    @NotNull
     private String uri;
+    @NotNull
     private String ip;
+    @NotNull
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
